2a756315dbe799e6677f78821224ac74d1adcc36
90houlaoheshang <907333918@qq.com>
2021-01-13 17:23:14 +0800

    添加测试

--- tests/test_BinanceFapiAsyncWs.py
+++ tests/test_BinanceFapiAsyncWs.py
@@ -1,18 +1,57 @@
 import asyncio as asyncio
 from asyncUnittest import AsyncTestCase
 from BinanceAsyncWebsocket import BinanceFapiAsyncWs
+import ccxt.async_support as ccxt
 
 
 class TestBinanceFapiAsyncWs(AsyncTestCase):
     @classmethod
     async def setUpClass(cls) -> None:
-        # cls.ws = BinanceFapiAsyncWs(input('Input apikey:'))
-        cls.ws = BinanceFapiAsyncWs('z9J4GCAR26SqfaHhiOIeB1b9LRPxdgrWwClFjyi2rcBmHMRyeUFQ9o6aanAnvmfx')
+        apikey = input('Input apikey:')
+        secret = input('Input secret:')
+
+        cls.rest = ccxt.binance(
+            {
+                "apiKey": apikey,
+                "secret": secret
+            }
+        )
+        cls.ws = BinanceFapiAsyncWs(apikey)
+        cls.msgs = []
+        cls.st = None
+
+        async def stream():
+            cls.st = cls.ws.stream_filter()
+            async for msg in cls.st:
+                cls.msgs.append(msg)
+
+        cls.stream_task = asyncio.create_task(stream())
 
     @classmethod
     async def tearDownClass(cls) -> None:
-        await asyncio.create_task(cls.ws.exit())
+        ws_exit_task = asyncio.create_task(cls.ws.exit())
+        rest_close_task = asyncio.create_task(cls.rest.close())
+        st_exit_close_task = asyncio.create_task(cls.st.close())
+        cls.stream_task.cancel()
+        await ws_exit_task
+        await rest_close_task
+        await st_exit_close_task
+        asyncio.get_running_loop().stop()
 
     async def test_generate_listenkey(self):
         lk = await asyncio.create_task(self.ws._generate_listenkey(True))
         self.assertIn('listenKey', lk)
+
+    async def test_open_cancel_order(self):
+        new_order = await asyncio.create_task(
+            self.rest.create_order(
+                symbol='BTC/USDT', side='BUY', type='LIMIT', price=10, amount=0.001, params={'type': 'future'}))
+        await asyncio.sleep(2)
+        self.assertTrue(any(
+            [(msg['e'] == "ORDER_TRADE_UPDATE" and msg['o']['x'] == 'NEW' and str(msg['o']['i']) == new_order['id']) for
+             msg in self.msgs]))
+        await asyncio.create_task(self.rest.cancel_order(new_order['id'], 'BTC/USDT', params={'type': 'future'}))
+        await asyncio.sleep(2)
+        self.assertTrue(any(
+            [(msg['e'] == "ORDER_TRADE_UPDATE" and msg['o']['x'] == 'CANCELED' and str(msg['o']['i']) ==
+              new_order['id']) for msg in self.msgs]))
