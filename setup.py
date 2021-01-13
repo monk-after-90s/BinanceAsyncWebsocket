23228a0302e6d4d181a9c5b97d3f0d2821d69199
90houlaoheshang <907333918@qq.com>
2021-01-13 18:22:40 +0800

    更新版本号

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="BinanceAsyncWebsocket",
-    version="1.0.4",
+    version="2.0.0",
     author="Antas",
     author_email="",
     description="Binance asynchronous websocket SDK",
